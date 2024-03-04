7e1a13f3af57783011abdb8b2e58569bb9c439ad
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-04 14:13:12 +0100

    added add function

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 // <---------------------------------------------------------------------------------------------------->
@@ -43,5 +44,41 @@ func Create(rootPath string) {
 				tempSlice = append(tempSlice, []string{entryPath, entry.Name(), filepath.Ext(entry.Name())})
 			}
 		}
+
+		EntrieFilesystem.add(&tempSlice, true)
+	}
+}
+
+func (fs *Filesystem) add(newSlice *[][]string, isMainDirs bool) {
+	if !isMainDirs {
+		return
+	}
+
+	for _, item := range *newSlice {
+		itemPath := item[0]
+		itemName := item[1]
+		itemExtension := item[2]
+
+		// trim file extensions from the name
+		if len(itemExtension) > 0 {
+			itemName = itemName[:len(itemName)-len(itemExtension)]
+		} else {
+			// if there is no file extension make the extension "File"
+			itemExtension = "File"
+		}
+
+		// check if the file type is already stored in the fs, if not add it in
+		if _, ok := fs.mainDirs[itemExtension]; !ok {
+			fs.mainDirs[itemExtension] = make(map[int]map[string][]interface{})
+		}
+
+		// check if the file length is already stored for the file extension, if not add it in
+		if _, ok := fs.mainDirs[itemExtension][len(itemName)]; !ok {
+			fs.mainDirs[itemExtension][len(itemName)] = make(map[string][]interface{})
+		}
+
+		// add the file into the fs at its length with the path as a key and the name as it's value
+		fs.mainDirs[itemExtension][len(itemName)][strings.ReplaceAll(itemPath, "\\", "/")] = []interface{}{itemName}
+		//TODO add binary here
 	}
 }
