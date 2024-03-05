24d77c618b35ffadb0e6424e313bec9abb94e7ba
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:11:50 +0100

    added FormatEntries

--- internal/util/util.go
+++ internal/util/util.go
@@ -38,3 +38,22 @@ func InsertUsername(pathInputs []string) ([]string, error) {
 
 	return pathInputs, nil
 }
+
+// FormatEntries replaces "\" with "/" and for folders adds a "/" at the end, if there isn't one already
+// This can't be used with a mix of files and folders
+func FormatEntries(entries []string, isFolder bool) []string {
+	for index, entry := range entries {
+		entries[index] = strings.ReplaceAll(entry, "\\", "/")
+
+		// if the inputs are just entries skip them
+		if !isFolder {
+			continue
+		}
+
+		if string(entry[len(entry)-1]) != "/" {
+			entries[index] = entries[index] + "/"
+		}
+	}
+
+	return entries
+}
