d14f779189a02e981fce9e18c3804e16a9690bb5
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:02:05 +0100

    added Init

---
+++ internal/setup/setup.go
@@ -0,0 +1,27 @@
+// Package setup ...
+package setup
+
+// <---------------------------------------------------------------------------------------------------->
+
+import (
+	"fmt"
+
+	"github.com/SkillpTm/better-windows-search/internal/cache"
+	"github.com/SkillpTm/better-windows-search/internal/config"
+)
+
+// <---------------------------------------------------------------------------------------------------->
+
+// Init sets up the BWSConfig and EntrieFilesystem
+func Init() error {
+	var err error
+
+	config.BWSConfig, err = config.New()
+	if err != nil {
+		return fmt.Errorf("couldn't assign BWSConfig; %s", err.Error())
+	}
+
+	cache.EntrieFilesystem = cache.New(&config.BWSConfig.Maindirs, true)
+
+	return nil
+}
