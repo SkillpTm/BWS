44ae23b20ae05ffb43370883dee118480f87159e
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:13:21 +0100

    added GetJSONData

--- internal/util/util.go
+++ internal/util/util.go
@@ -4,7 +4,10 @@ package util
 // <---------------------------------------------------------------------------------------------------->
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 	"os/user"
 	"strings"
 )
@@ -56,4 +59,38 @@ func FormatEntries(entries []string, isFolder bool) []string {
 	}
 
 	return entries
+}
+
+// GetJSONData will provide a map with JSON data of the provided file
+func GetJSONData(filePath string) (map[string]interface{}, error) {
+	// define return var early
+	var jsonData map[string]interface{}
+	var returnErr error = nil
+
+	jsonFile, err := os.Open(filePath)
+	if err != nil {
+		return jsonData, fmt.Errorf("couldn't open JSON file (%s); %s", filePath, err.Error())
+	}
+
+	// defer close the file with error handling
+	defer func() {
+		err = jsonFile.Close()
+		if err != nil {
+			returnErr = fmt.Errorf("couldn't close JSON file (%s); %s", filePath, jsonFile.Close())
+		}
+	}()
+
+	defer jsonFile.Close()
+
+	rawJSONData, err := io.ReadAll(jsonFile)
+	if err != nil {
+		return jsonData, fmt.Errorf("couldn't read JSON file (%s); %s", filePath, err.Error())
+	}
+
+	err = json.Unmarshal(rawJSONData, &jsonData)
+	if err != nil {
+		return jsonData, fmt.Errorf("couldn't unmarshal raw JSON data; %s", err.Error())
+	}
+
+	return jsonData, returnErr
 }
