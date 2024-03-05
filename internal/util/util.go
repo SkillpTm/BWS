044ef5b07f8106bcae4ea86b6ed663e91d0680bc
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:06:56 +0100

    added InsertUsername

--- internal/util/util.go
+++ internal/util/util.go
@@ -3,6 +3,12 @@ package util
 
 // <---------------------------------------------------------------------------------------------------->
 
+import (
+	"fmt"
+	"os/user"
+	"strings"
+)
+
 // <---------------------------------------------------------------------------------------------------->
 
 // ConvertSliceInterface converts a []interface{} to a slice of any type
@@ -14,4 +20,21 @@ func ConvertSliceInterface[T any](sliceInput []interface{}) []T {
 	}
 
 	return newSlice
+}
+
+// InsertUsername replaces <USERNAME> in any path to the actual username of the current user
+func InsertUsername(pathInputs []string) ([]string, error) {
+	// Get the current user for their name
+	currentUser, err := user.Current()
+	if err != nil {
+		return pathInputs, fmt.Errorf("couldn't get current user for username; %s", err.Error())
+	}
+
+	username := strings.Split(currentUser.Username, "\\")[1]
+
+	for index, path := range pathInputs {
+		pathInputs[index] = strings.ReplaceAll(path, "<USERNAME>", username)
+	}
+
+	return pathInputs, nil
 }
