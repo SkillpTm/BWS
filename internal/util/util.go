829d894769bc8117294e636e595f523ca3db3d2f
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:04:19 +0100

    added ConvertSliceInterface

--- internal/util/util.go
+++ internal/util/util.go
@@ -1,2 +1,17 @@
 // Package util ...
 package util
+
+// <---------------------------------------------------------------------------------------------------->
+
+// <---------------------------------------------------------------------------------------------------->
+
+// ConvertSliceInterface converts a []interface{} to a slice of any type
+func ConvertSliceInterface[T any](sliceInput []interface{}) []T {
+	newSlice := []T{}
+
+	for _, item := range sliceInput {
+		newSlice = append(newSlice, item.(T))
+	}
+
+	return newSlice
+}
