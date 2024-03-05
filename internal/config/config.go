414ad49af8ea95a233e95c284a3c0eeec5ff1405
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-05 17:00:23 +0100

    added config struct and New function

---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+// Package config ...
+package config
+
+import (
+	"fmt"
+
+	"github.com/SkillpTm/better-windows-search/internal/util"
+)
+
+// <---------------------------------------------------------------------------------------------------->
+
+var BWSConfig *Config
+
+// <---------------------------------------------------------------------------------------------------->
+
+type Config struct {
+	Maindirs           []string
+	ExcludeSubMainDirs []string
+	ExcludeDirs        []string
+}
+
+// <---------------------------------------------------------------------------------------------------->
+
+// New creates a new Conifg struct with the values from ./configs/config.json
+func New() (*Config, error) {
+	newConfig := Config{}
+
+	configMap, err := util.GetJSONData("./configs/config.json")
+	if err != nil {
+		return &newConfig, fmt.Errorf("couldn't open config JSON file; %s", err.Error())
+	}
+
+	index := 0
+
+	// populate the newConfig with properly formated strings
+	for _, value := range configMap {
+		newSlice := util.ConvertSliceInterface[string](value.([]interface{}))
+		newSlice = util.FormatFileEntries(newSlice, true)
+		newSlice, err = util.InsertUsername(newSlice)
+		if err != nil {
+			return &newConfig, fmt.Errorf("couldn't replace '<USERNAME>'; %s", err.Error())
+		}
+
+		switch index {
+		case 0:
+			newConfig.Maindirs = newSlice
+		case 1:
+			newConfig.ExcludeSubMainDirs = newSlice
+		case 2:
+			newConfig.ExcludeDirs = newSlice
+		}
+
+		index++
+	}
+
+	return &newConfig, nil
+}
