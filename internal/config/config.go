1c621b67407ffe456f35f391158878907b023c82
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-06 17:39:37 +0100

    added ExcludeDirsByName

--- internal/config/config.go
+++ internal/config/config.go
@@ -17,25 +17,35 @@ type Config struct {
 	Maindirs           []string
 	ExcludeSubMainDirs []string
 	ExcludeDirs        []string
+	ExcludeDirsByName  []string
 }
 
 // <---------------------------------------------------------------------------------------------------->
 
-// New creates a new Conifg struct with the values from ./../configs/config.json
+// New creates a new Conifg struct with the values from ./configs/config.json
 func New() (*Config, error) {
 	newConfig := Config{}
 
-	configMap, err := util.GetJSONData("./../configs/config.json")
+	configMap, err := util.GetJSONData("./configs/config.json")
 	if err != nil {
 		return &newConfig, fmt.Errorf("couldn't open config JSON file; %s", err.Error())
 	}
 
 	index := 0
 
-	// populate the newConfig with properly formated strings
+	// populate the newConfig with properly formated paths
 	for _, value := range configMap {
 		newSlice := util.ConvertSliceInterface[string](value.([]interface{}))
-		newSlice = util.FormatEntries(newSlice, true)
+
+		// on index 3 we  only want the slice of strings and not format them, since they aren't paths
+		if index == 3 {
+			newConfig.ExcludeDirsByName = newSlice
+			continue
+		}
+
+		for index, element := range newSlice {
+			newSlice[index] = util.FormatEntry(element, true)
+		}
 		newSlice, err = util.InsertUsername(newSlice)
 		if err != nil {
 			return &newConfig, fmt.Errorf("couldn't replace '<USERNAME>'; %s", err.Error())
