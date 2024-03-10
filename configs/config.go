403363651a4949ddafe73893dc5ae69ac89725be
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-10 04:48:43 +0100

    changed froma  config.json to a config.go

---
+++ configs/config.go
@@ -0,0 +1,51 @@
+// Package defaultconfig ...
+package defaultconfig
+
+// <---------------------------------------------------------------------------------------------------->
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// <---------------------------------------------------------------------------------------------------->
+
+var rawJSONData = []byte(`
+{
+    "cpuThreads": 4,
+    "mainDirs": [
+        "C:/Users/<USERNAME>/",
+    ],
+    "excludeSubMainDirs": [
+        "C:/Users/<USERNAME>/AppData/Roaming"
+    ],
+    "secondaryDirs": [
+        "C:/"
+    ],
+    "excludeDirs": [
+        "C:/Windows/",
+        "C:/$Recycle.Bin/",
+        "C:/Users/<USERNAME>/AppData/Local",
+        "C:/Users/<USERNAME>/AppData/LocalLow"
+    ],
+    "excludeDirsByName": [
+        ".git",
+        "bin",
+        "node_modules",
+        "steamapps"
+    ]
+}`)
+
+// <---------------------------------------------------------------------------------------------------->
+
+// Get will provide a map with the config JSON data
+func Get() (map[string]interface{}, error) {
+	var jsonData map[string]interface{}
+
+	err := json.Unmarshal(rawJSONData, &jsonData)
+	if err != nil {
+		return jsonData, fmt.Errorf("couldn't unmarshal raw JSON data; %s", err.Error())
+	}
+
+	return jsonData, nil
+}
