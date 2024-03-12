637fe49ab517570e27e4560b8eb122a08829ec3c
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-12 03:20:48 +0100

    added SetupProperly functionality and removed setup.Init

--- bws.go
+++ bws.go
@@ -1,29 +1,24 @@
-// Package bws ...
+// Package bws containes the main Search function and start up logic of bws.
 package bws
 
 // <---------------------------------------------------------------------------------------------------->
 
 import (
-	"log"
 	"time"
 
 	"github.com/skillptm/bws/internal/cache"
 	"github.com/skillptm/bws/internal/config"
 	"github.com/skillptm/bws/internal/search"
-	"github.com/skillptm/bws/internal/setup"
 )
 
 // <---------------------------------------------------------------------------------------------------->
 
+// init executes as soon as bws is imported into another project and launches a goroutine of updateCache
 func init() {
-	err := setup.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	go updateCache()
 }
 
+// updateCache updates with teh use of tickers both the MainDirs and the SecondaryDirs
 func updateCache() {
 	// create tickers for how often the FileSystem components are supossed to update
 	mainDirsTicker := time.NewTicker(3 * time.Minute)
@@ -35,11 +30,19 @@ func updateCache() {
 	for {
 		select {
 		case <-mainDirsTicker.C:
+			if !cache.EntrieFilesystem.SetupProperly {
+				continue
+			}
+
 			// check if the Filesystem is Updateable, if so update it, otherwise wait for the next cycle
 			if cache.EntrieFilesystem.Updateable {
-				cache.EntrieFilesystem.Update(config.BWSConfig.Maindirs, true)
+				cache.EntrieFilesystem.Update(config.BWSConfig.MainDirs, true)
 			}
 		case <-secondaryDirsTicker.C:
+			if !cache.EntrieFilesystem.SetupProperly {
+				continue
+			}
+
 			// check if the Filesystem is Updateable, if so update it, otherwise wait for the next cycle
 			if cache.EntrieFilesystem.Updateable {
 				cache.EntrieFilesystem.Update(config.BWSConfig.SecondaryDirs, false)
@@ -48,11 +51,20 @@ func updateCache() {
 	}
 }
 
-// Search takes in any substring that you want to search for through all filenames. You may add any amount if file extensions as well.
-// The extendedSearch flag dictates, if we search through the SecondaryDirs from the ./configs/config.json file.
-// To change the folders included/excluded in the search edit the ./configs/config.json file.
-// This function is a wrapper around the search and rank functions and returns the ranked search results at the end.
+/*
+Search takes in any substring that you want to search for through all filenames. You may add any amount of file extensions as well.
+The extendedSearch flag dictates, if we search through the SecondaryDirs.
+To change the folders included/excluded in the search use the pkg/options set functions.
+This function is a wrapper around the search and rank functions and returns the ranked search results at the end.
+
+On it's first execution the function will take longer, as it needs to generate the cache first.
+*/
 func Search(searchString string, fileExtensions []string, extendedSearch bool) []string {
+	// check if the FileSystem is setup properly, if not reset it by regenerating it
+	if !cache.EntrieFilesystem.SetupProperly {
+		cache.EntrieFilesystem = cache.New(config.BWSConfig.MainDirs, config.BWSConfig.SecondaryDirs)
+	}
+
 	// wait for the FileSystem to be readable
 	for {
 		if cache.EntrieFilesystem.Readable {
