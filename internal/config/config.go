e171cbb93d5d43d1f30448ba8c160262ec8b0c1c
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-12 03:17:14 +0100

    added DefaultConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -5,8 +5,9 @@ package config
 
 import (
 	"fmt"
+	"math"
+	"runtime"
 
-	defaultconfig "github.com/skillptm/bws/configs"
 	"github.com/skillptm/bws/internal/util"
 )
 
@@ -14,11 +15,39 @@ import (
 
 var BWSConfig *Config
 
+var DefaultConfig = map[string]interface{}{
+	"cpuThreads": int(math.Ceil(float64(runtime.NumCPU()) / float64(4))),
+	"mainDirs": []string{
+		"C:/Users/<USERNAME>/",
+		"E:/$MyData",
+		"E:/mssecuredisk.setup",
+	},
+	"excludeSubMainDirs": []string{
+		"C:/Users/<USERNAME>/AppData/Roaming",
+	},
+	"secondaryDirs": []string{
+		"C:/",
+		"D:/",
+	},
+	"excludeDirs": []string{
+		"C:/Windows/",
+		"C:/$Recycle.Bin/",
+		"C:/Users/<USERNAME>/AppData/Local",
+		"C:/Users/<USERNAME>/AppData/LocalLow",
+	},
+	"excludeDirsByName": []string{
+		".git",
+		"bin",
+		"node_modules",
+		"steamapps",
+	},
+}
+
 // <---------------------------------------------------------------------------------------------------->
 
 type Config struct {
 	CPUThreads         int
-	Maindirs           []string
+	MainDirs           []string
 	ExcludeSubMainDirs []string
 	SecondaryDirs      []string
 	ExcludeDirs        []string
@@ -28,13 +57,8 @@ type Config struct {
 // <---------------------------------------------------------------------------------------------------->
 
 // New creates a new Conifg struct with the values from ./configs/config.json
-func New() (*Config, error) {
+func New(configMap map[string]interface{}) (*Config, error) {
 	newConfig := Config{}
-
-	configMap, err := defaultconfig.Get()
-	if err != nil {
-		return &newConfig, fmt.Errorf("couldn't get JSON config data; %s", err.Error())
-	}
 
 	newConfig.CPUThreads = int(configMap["cpuThreads"].(float64))
 	delete(configMap, "cpuThreads")
@@ -46,14 +70,14 @@ func New() (*Config, error) {
 		for index, element := range newSlice {
 			newSlice[index] = util.FormatEntry(element, true)
 		}
-		newSlice, err = util.InsertUsername(newSlice)
+		newSlice, err := util.InsertUsername(newSlice)
 		if err != nil {
 			return &newConfig, fmt.Errorf("couldn't replace '<USERNAME>'; %s", err.Error())
 		}
 
 		switch key {
 		case "mainDirs":
-			newConfig.Maindirs = newSlice
+			newConfig.MainDirs = newSlice
 		case "excludeSubMainDirs":
 			newConfig.ExcludeSubMainDirs = newSlice
 		case "secondaryDirs":
