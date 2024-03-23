cda19492f0787c1351f6e4b5c37c3409ce85f6da
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2024-03-23 02:05:54 +0100

    added bool return value to indicate if the fcuntion was broken early

--- bws.go
+++ bws.go
@@ -61,9 +61,10 @@ func updateCache() {
 /*
 baseSearch is a wrapper around the search and rank functions and returns the ranked search results at the end.
 
-Additionally if baseSearch was launched by GoSearchWithBreak it can be stopped at any point with the forceStopChan
+Additionally if baseSearch was launched by GoSearchWithBreak it can be stopped at any point with the forceStopChan.
+If the search was stopped early we return a true, otherwise false.
 */
-func baseSearch(searchString string, fileExtensions []string, extendedSearch bool, forceStopChan chan bool) []string {
+func baseSearch(searchString string, fileExtensions []string, extendedSearch bool, forceStopChan chan bool) ([]string, bool) {
 	// check if the FileSystem is setup properly, if not reset it by regenerating it
 	if !cache.EntrieFilesystem.SetupProperly {
 		ForceUpdateCache()
@@ -89,18 +90,18 @@ func baseSearch(searchString string, fileExtensions []string, extendedSearch boo
 
 	// check if we have to stop the baseSearch
 	if len(forceStopChan) > 0 {
-		return []string{}
+		return []string{}, true
 	}
 
 	output := *search.Rank(results, pattern, forceStopChan)
 
 	// check if we have to stop the baseSearch
 	if len(forceStopChan) > 0 {
-		return []string{}
+		return []string{}, true
 	}
 
 	// rank and sort the files
-	return output
+	return output, false
 }
 
 /*
@@ -111,21 +112,22 @@ To change the folders included/excluded in the search use the pkg/options set fu
 On it's first execution the function will take longer, as it needs to generate the cache first.
 */
 func Search(searchString string, fileExtensions []string, extendedSearch bool) []string {
-	return baseSearch(searchString, fileExtensions, extendedSearch, make(chan bool, 1)) // insert a dummy channel, as it's not needed here
+	results, _ := baseSearch(searchString, fileExtensions, extendedSearch, make(chan bool, 1)) // insert a dummy channel, as it's not needed here
+	return results
 }
 
 /*
 GoSearchWithBreak behaves exactly like Search, the only difference is, it requires a break channel as an input.
 
 This function should be started as a goroutine and it can be cancelled early by sending something in the breakChan.
+If it breaks early, it returns a true, otherwise false.
 */
-func GoSearchWithBreak(searchString string, fileExtensions []string, extendedSearch bool, breakChan chan bool) []string {
+func GoSearchWithBreak(searchString string, fileExtensions []string, extendedSearch bool, breakChan chan bool) ([]string, bool) {
 	forceStopChan := make(chan bool, 1)
 
 	go func() {
 		// check for when we receive the break signal
 		for range breakChan {
-			close(breakChan)
 			// send something into the foreStopChan to stop the baseSearch
 			forceStopChan <- true
 		}
